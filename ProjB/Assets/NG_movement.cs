97597b4fda6b6787ecdb5f18ce3a90226a8880ea
thomas <tmorg25052@gmail.com>
2020-05-24 18:29:36 -0400

    idles in the right direction

--- ProjB/Assets/NG_movement.cs
+++ ProjB/Assets/NG_movement.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class NG_movement : MonoBehaviour
@@ -8,23 +11,26 @@ public class NG_movement : MonoBehaviour
     public Rigidbody2D rb;
 
     Vector2 movement;
+    Vector2 lastMovement;
 
     public Animator animator;
 
+    private Camera cam;
+
     private void Start()
     {
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         //input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
 
-        animator.SetFloat("horizontal", movement.x);
-        animator.SetFloat("vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        handleMovement();
+        
+
+        
 
     }
 
@@ -32,5 +38,41 @@ public class NG_movement : MonoBehaviour
     {
         //movement 
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void handleMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x  != 0 || movement.y != 0)
+        {
+            lastMovement.x = movement.x;
+            lastMovement.y = movement.y;
+        }
+        else
+        {
+            animator.SetFloat("lastHorizontal", lastMovement.x);
+            animator.SetFloat("lastVertical", lastMovement.y);
+        }
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        
+    }
+
+    void mouseDirection()
+    {
+        Vector3 mouse = Input.mousePosition;
+
+        Vector3 screenpoint = cam.WorldToScreenPoint(transform.localPosition);
+
+        Vector2 offset = new Vector2(mouse.x - screenpoint.x, mouse.y - screenpoint.y);
+
+
+
+        print(offset);
     }
 }
